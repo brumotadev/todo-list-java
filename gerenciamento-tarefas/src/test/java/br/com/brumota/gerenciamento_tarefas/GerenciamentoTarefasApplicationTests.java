a2856d27781d9f6189bed530130e04701891e418
Bruno Mota <bmotadev@gmail.com>
2024-08-11 15:30:03 -0300

    Testes automatizados - Adicionado webflux

--- gerenciamento-tarefas/src/test/java/br/com/brumota/gerenciamento_tarefas/GerenciamentoTarefasApplicationTests.java
+++ gerenciamento-tarefas/src/test/java/br/com/brumota/gerenciamento_tarefas/GerenciamentoTarefasApplicationTests.java
@@ -1,13 +1,25 @@
 package br.com.brumota.gerenciamento_tarefas;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.com.brumota.gerenciamento_tarefas.entity.Todo;
 
 @SpringBootTest
 class GerenciamentoTarefasApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testeCreateTodoSuccess() {
+		var todo = new Todo("todo1", "desc todo 1", false, 1);
+	}
+
+	@Test
+	void testCreateTodoFailure() {
+
 	}
 
 }
